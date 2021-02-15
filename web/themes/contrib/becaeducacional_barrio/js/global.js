2005fa1c2414c5eac91a1c84d302bfa6c39449c9
Antonio Hernandez Souza <ahernandezsouza@gmail.com>
2021-02-14 19:54:16 -0600

    Adjustar Templates Direccion

--- web/themes/contrib/becaeducacional_barrio/js/global.js
+++ web/themes/contrib/becaeducacional_barrio/js/global.js
@@ -10,6 +10,41 @@
   Drupal.behaviors.becaeducacional_barrio = {
     attach: function (context, settings) {
 
+      $( "input#edit-payment-information-add-payment-method-billing-information-field-desea-factura-value" ).change( function() {
+        if($( this ).is(":checked")){
+          console.log($( this ).is(":checked"));
+          $('div#edit-payment-information-add-payment-method-billing-information-field-rfc-wrapper').show();
+          $('div#edit-payment-information-add-payment-method-billing-information-field-direccion-fiscal-wrapper').show();
+        }
+        else{
+          $('div#edit-payment-information-add-payment-method-billing-information-field-rfc-wrapper').hide();
+          $('div#edit-payment-information-add-payment-method-billing-information-field-direccion-fiscal-wrapper').hide();
+        }
+      });
+
+      $( document ).ready( function() {
+        if($("form#profile-customer-address-book-edit-form input#edit-field-desea-factura-value").length != 0){
+          if($("form#profile-customer-address-book-edit-form input#edit-field-desea-factura-value").is(":checked")){
+            console.log($( this ).is(":checked"));
+            $('fieldset#datos-fiscales').show();
+          }
+          else{
+            $('fieldset#datos-fiscales').hide();
+          }
+        }
+        });
+
+
+      $( "form#profile-customer-address-book-edit-form input#edit-field-desea-factura-value" ).change( function() {
+        if($( this ).is(":checked")){
+          console.log($( this ).is(":checked"));
+          $('fieldset#datos-fiscales').show();
+        }
+        else{
+          $('fieldset#datos-fiscales').hide();
+        }
+      });
+
     }
   };
 
